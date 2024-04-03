e913d9026f9df4fa91c54333a1b2064134023452
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 <christopherkilkes@gmail.com>
2024-04-03 04:45:44 -0700

    Create api_keys.py

---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key - always include the quotes when placing API keys in config file
+weather_api_key = "8589a43e2a1672fcc9955e651825a1c7"
+
+# Geoapify API Key
+geoapify_key = "7f62139a5af9436e92bb29a7ee450be8"
